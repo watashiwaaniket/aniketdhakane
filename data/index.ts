65607902c7a3ebff66a629623b00be7d4b95b150
Aniket Dhakane <aniketdhakane3@gmail.com>
2024-11-02 17:08:59 +0530

    about section blueprint init

---
+++ data/index.ts
@@ -0,0 +1,123 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I Consider Communication as the Best Asset for Collaboration",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "In a boat surfing the world of code",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Always Eager to learn how a technology works",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a UI Component Library",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Hiku-App",
+      des: "A Next.js Web Application to build and customize your own hikus, powered by cloudinary AI and Mongo Atlas",
+      img: "/p1.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://hiku-app.vercel.app",
+    },
+    {
+      id: 2,
+      title: "Component Cloud",
+      des: "An opensource UI components library where users can submit their own code or use others code for their websites. Made with Next.js, database by supabase, secured by nextAuth and powered by vercel",
+      img: "/p2.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://github.com/watashiwaaniket/component-cloud",
+    },
+    {
+      id: 3,
+      title: "Weather Alert",
+      des: "A MERN stack weather alert application which would give weather forcast everyday at  the users email and also tips on water harvesting.",
+      img: "/p3.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/watashiwaaniket/oomdMiniProject",
+    },
+  ];
+  
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Software Developer Intern",
+      desc: "Worked at XpedioLive Technologies pvt. ltd. as a fullstack web developer, working with Svelte Framework",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
