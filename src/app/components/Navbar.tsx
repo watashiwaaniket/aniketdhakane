5119af19e4614ac28176fb9e09f1c0b21982b484
watashiwaaniket <aniketdhakane3@gmail.com>
2025-07-23 10:38:27 +0530

    responsiveness for mobile

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import GradientText from "./ui-components/GradientText";
 
 export function Navbar(){
     return(
-        <div className="flex space-x-2 p-6 justify-end w-full md:text-xl">
+        <div className="flex space-x-2 py-4 md:p-6 justify-end w-full md:text-xl">
             <Link href={'/'}><p className="hover:underline">projects</p></Link>
             <Link href={'/'}><p className="hover:underline">blog</p></Link>
             <GradientText
