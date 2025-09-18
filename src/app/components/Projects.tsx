e6c83d5b7209c2451908b5779626948c12ce6dd8
watashiwaaniket <aniketdhakane3@gmail.com>
2025-09-18 09:44:57 +0530

    added more projects

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,17 +1,49 @@
 'use client';
 
-import { MailIcon } from "./icons/MailIcon";
 import { ProjectCard } from "./ui-components/ProjectCard";
-import projects from "./utils/lib";
+import { frontendProjects, fullstackProjects, miscProjects } from "./utils/lib";
 
 export function Projects(){
-    console.log(projects[1].desc)
+    console.log(fullstackProjects[1].desc)
     return(
         <div className="my-4">
             <h1 className="text-lg font-bold">Projects</h1>
-            <div className="flex flex-wrap justify-center items-center">
-                {Object.keys(projects).map((key) => {
-                    const project = projects[Number(key)];
+            <h2 className="text-sm font-semibold p-1">Fullstack Projects</h2>
+            <div className="flex flex-wrap items-center">
+                {Object.keys(fullstackProjects).map((key) => {
+                    const project = fullstackProjects[Number(key)];
+                    const IconComponent = project.icon;
+                    return(
+                        <ProjectCard
+                            key={key}
+                            icon={<IconComponent />}
+                            title={project.title}
+                            desc={project.desc}
+                            link={project.link}
+                        />
+                    )
+                })}
+            </div>
+            <h2 className="text-sm font-semibold p-1">Frontend Projects</h2>
+            <div className="flex flex-wrap items-center">
+                {Object.keys(frontendProjects).map((key) => {
+                    const project = frontendProjects[Number(key)];
+                    const IconComponent = project.icon;
+                    return(
+                        <ProjectCard
+                            key={key}
+                            icon={<IconComponent />}
+                            title={project.title}
+                            desc={project.desc}
+                            link={project.link}
+                        />
+                    )
+                })}
+            </div>
+            <h2 className="text-sm font-semibold p-1">Misc Projects</h2>
+            <div className="flex flex-wrap items-center">
+                {Object.keys(miscProjects).map((key) => {
+                    const project = miscProjects[Number(key)];
                     const IconComponent = project.icon;
                     return(
                         <ProjectCard
--- src/app/components/utils/lib.ts
+++ src/app/components/utils/lib.ts
@@ -1,4 +1,4 @@
-import { PersonaIcon, CodeSeekIcon, OsapIcon, HikuIcon } from "../icons/ProjectIcons";
+import { PersonaIcon, CodeSeekIcon, OsapIcon, HikuIcon, ServoIcon, PoemsForHerIcon } from "../icons/ProjectIcons";
 
 interface Project {
   title: string;
@@ -11,7 +11,7 @@ interface Projects {
   [key: number]: Project;
 }
 
-const projects: Projects = {
+export const fullstackProjects: Projects = {
   1: {
     title: 'PersonAi',
     desc: 'Ditch the dull notes. Meet PersonAi: your smart, free second brain that remembers, summarizes, and thinks with you.',
@@ -19,18 +19,12 @@ const projects: Projects = {
     icon: PersonaIcon,
   },
   2: {
-    title: 'CodeSeek-r1',
-    desc: 'A VSCode extension leveraging the power of deepseek-r1 running locally, while maintaining user privacy',
-    link: 'https://github.com/watashiwaaniket/codeseek',
-    icon: CodeSeekIcon,
-  },
-  3: {
     title: 'osap',
     desc: 'Platform catering to open source contributors to find the next interesting project to tinker around with',
     link: 'https://osaplfm.vercel.app/',
     icon: OsapIcon,
   },
-  4: {
+  3: {
     title: 'Hiku App',
     desc: 'A sample Hiku generating application which uses Cloudinary AI image generation for generating the cards',
     link: 'https://hiku-app.vercel.app/',
@@ -38,4 +32,28 @@ const projects: Projects = {
   }
 };
 
-export default projects;
+export const frontendProjects: Projects = {
+  1: {
+    title: 'Servo UI',
+    desc: 'Landing page of a UI component library made using shaders and motion',
+    link: 'https://servoui.vercel.app/',
+    icon: ServoIcon
+  },
+  2: {
+    title: 'Poems For Her',
+    desc: "Poems which I once wrote for her.",
+    link: 'https://poemsforher.vercel.app/',
+    icon: PoemsForHerIcon
+  }
+}
+
+export const miscProjects: Projects = {
+  1: {
+    title: 'CodeSeek-r1',
+    desc: 'A VSCode extension leveraging the power of deepseek-r1 running locally, while maintaining user privacy',
+    link: 'https://github.com/watashiwaaniket/codeseek',
+    icon: CodeSeekIcon,
+  }
+}
+
+
