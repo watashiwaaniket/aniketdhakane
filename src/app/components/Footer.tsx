fbe03808b54ee263e53ad8c8223327b14b015bd0
watashiwaaniket <aniketdhakane3@gmail.com>
2025-09-21 19:56:13 +0530

    added another fav song

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
 import ShinyText from "./ui-components/ShinyText";
+import Image from "next/image";
 
 export default function Footer(){
     return(
         <div className="text-center">
             <h1 className="text-lg font-bold py-4">All time Fav Songs</h1>
-            <div className="w-full flex justify-center pb-8">
+            <div className="w-full flex justify-center pb-8 gap-8">
             <Link href={'https://open.spotify.com/track/7mykoq6R3BArsSpNDjFQTm?si=d604b24bb4f84564'}>
-                <img src="/cp.png" alt="" className="w-60 rounded-xl" />
+                <Image src='/cp.png' alt="" height={240} width={240} className="rounded-xl" />
+            </Link>
+            <Link href={'https://music.apple.com/in/album/after-hours/1505683705?i=1505683998'}>
+                <Image src='/ah.jpg' alt="" height={240} width={240} className="rounded-xl"/>
             </Link>
             </div>
             <hr className="text-gray-500"/>
