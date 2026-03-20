c8a1f62a4a19d391c7bd8ae4beaa768625fc7024
watashiwaaniket <aniketdhakane3@gmail.com>
2026-03-20 20:04:49 +0530

    updated resume

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,10 +7,14 @@ export function Navbar() {
   return (
     <div className="flex space-x-4 py-4 md:p-6 justify-end w-full text-sm">
       <Link href={"/wall"}>
-        <p className="border px-4 border-neutral-800 rounded-lg hover:bg-neutral-900">wall</p>
+        <p className="border px-4 border-neutral-800 rounded-lg hover:bg-neutral-900">
+          wall
+        </p>
       </Link>
       <Link href={"https://dev.to/hisukurifu"}>
-        <p className="border px-4 border-neutral-800 rounded-lg hover:bg-neutral-900">blog</p>
+        <p className="border px-4 border-neutral-800 rounded-lg hover:bg-neutral-900">
+          blog
+        </p>
       </Link>
       <GradientText
         colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
@@ -18,7 +22,7 @@ export function Navbar() {
         showBorder={true}
         className="custom-class"
       >
-        <Link href={"/aniket-dhakane-latest.pdf"}>resume</Link>
+        <Link href={"/aniketdhakane.pdf"}>resume</Link>
       </GradientText>
     </div>
   );
